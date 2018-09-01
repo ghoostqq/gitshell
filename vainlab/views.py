c736dc7610ab59fb812b7d730d9f9cc0d4348b2f
ghoostqq <qiu.gits@gmail.com>
2018-09-02 03:39:53 +0900

    Minor change comments

--- vainlab/views.py
+++ vainlab/views.py
@@ -52,7 +52,7 @@ def play_log(request, name):
     # See if name is in Player
     # If exists, get the Player
     # If not (means first visit), use api to request Player
-    # Then, render using the Player
+    # Then, use the Player to return and render
     # At the same time, use api to request Matches
     # Then return json, while saving to the db
 
